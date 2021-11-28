4e07b076434b9dbd8e717f50f9789eb7a50d5485
Youssef El-Nemaky <youssef.mmdoh99@gmail.com>
2021-11-28 15:21:45 +0200

    Update main function
    take input from the user and stop when the buffer is full. when the
    buffer is full just change the cursor from normal to blinking to indicate
    that the buffer is full to the user just like casio calculators.

--- code/main.c
+++ code/main.c
@@ -28,10 +28,10 @@ int main(void){
     uint8 bufferSizeCounter = 0;
     uint8 pressedKey;
     uint8 cursorIndex = 0;
+    bool doneLastShift = FALSE;
     
     LCD_init();
     LCD_sendCommand(LCD_CMD_DISP_ON_CURSOR_ON);
-
     while(1){
         /* get the pressed key from the keypad and save it into pressedKey variable */
         pressedKey = KEYPAD_getPressedKey();
@@ -40,7 +40,7 @@ int main(void){
         LCD_goToRowColumn(0, cursorIndex);
         if (pressedKey >= 0 && pressedKey <= 9) {
             /* Make sure the buffer is not full */
-            if(bufferSizeCounter < 40){
+            if(bufferSizeCounter < 39){
                 /* Display the pressed key on the LCD */
                 LCD_displayCharacter(pressedKey + '0');
                 /* save the pressed key into the input buffer */
@@ -48,7 +48,7 @@ int main(void){
             }
         } else {
             /* Make sure the buffer is not full */
-            if(bufferSizeCounter < 40){
+            if(bufferSizeCounter < 39){
                 /* Display the pressed key on the LCD */
                 LCD_displayCharacter(pressedKey);
                 /* save the pressed key into the input buffer */
@@ -56,22 +56,28 @@ int main(void){
             }
 
         }
-        /* update cursor index*/
-        cursorIndex++;
-        /* update buffer size */
-        bufferSizeCounter++;
+        if(bufferSizeCounter < 39){
+            /* update cursor index*/
+            cursorIndex++;
+            /* update buffer size */
+            bufferSizeCounter++;
+        }
         
         /* Shift the LCD to the right when it's full */
-        if (cursorIndex > 16)
-        {
+        if (cursorIndex >= 16 && bufferSizeCounter < 39){
             LCD_sendCommand(LCD_CMD_SHIFT_DISPLAY_LEFT);
         }
         _delay_ms(250);
 
         /* Display a warning message to the user indicating that the buffer is full */
-        if(bufferSizeCounter >= 40){
-            LCD_sendCommand(LCD_CMD_RETURN_HOME);
-            LCD_displayStringRowColumn("Max Buffer!", 1, 0);
+        if(bufferSizeCounter == 39){
+        	if(doneLastShift == FALSE){
+        		doneLastShift = TRUE;
+        		LCD_sendCommand(LCD_CMD_SHIFT_DISPLAY_LEFT);
+        	}
+            LCD_sendCommand(LCD_CMD_DISP_ON_CURSOR_BLINK);
+        } else {
+            LCD_sendCommand(LCD_CMD_DISP_ON_CURSOR_ON);
         }
     }
     return 0;
